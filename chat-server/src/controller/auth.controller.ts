fe2cabf9f315d5f472d3b18e5e7bf36792f38e4a
keshan200 <sachithakeshan175@gmail.com>
2025-08-17 13:56:45 +0530

    add user login with authentication and session handling

--- chat-server/src/controller/auth.controller.ts
+++ chat-server/src/controller/auth.controller.ts
@@ -1,5 +1,5 @@
 import {Request, Response, NextFunction } from "express";
-import jwt from "jsonwebtoken"
+import jwt, { JsonWebTokenError, JwtPayload, TokenExpiredError } from "jsonwebtoken"
 import { userModel } from "../models/UserModel";
 import { APIError } from "../errors/ApiErros";
 import bcrypt from "bcrypt"
@@ -62,3 +62,167 @@ export const signUp =  async (req:Request ,res :Response , next:NextFunction) =>
     }
 
 } 
+
+
+export const login  = async(req:Request,res:Response,next:NextFunction) => {
+
+     try{
+
+       const {email,password} = req.body
+       const user =  await userModel.findOne({email})
+       
+       if(!user){
+        throw new APIError(404,"user not found")
+       }
+
+       const isMatch = await bcrypt.compare(password,user.password)
+
+       if(!isMatch){
+         throw new APIError(401,"Invalid Credentials")
+       }
+
+       const AccessToken =  create_access_token(user._id.toString())
+       const RefreshToken =  create_refresh_token(user._id.toString())
+
+       console.log(RefreshToken)
+
+       const isProd =  process.env.NODE_ENV === "production"
+
+       res.cookie("refreshToken",RefreshToken,{
+          httpOnly:true,
+          secure:isProd,
+          sameSite:isProd ? "strict":"lax" ,
+          maxAge:7 * 24 * 60 * 60 * 1000,
+          path:"/api/auth/refresh-token"
+       })
+
+
+
+     res.cookie("User", JSON.stringify({
+          id: user._id,
+          full_name: user.full_name,
+          email: user.email,
+          img: user.img,
+
+      }), {
+
+         httpOnly: false,  
+         secure: isProd,
+         sameSite: isProd ? "strict" : "lax",
+         maxAge: 7 * 24 * 60 * 60 * 1000,
+         path: "/"
+      });
+
+
+
+       const userWithoutPass = {
+           id:user.id,
+           full_name:user.full_name,
+           img:user.img,
+           email:user.email,
+           AccessToken
+       }
+
+      
+
+       res.status(201).json(userWithoutPass)
+
+     }catch(error){
+          next(error)
+     }
+
+}
+
+
+
+export const refreshToken = async (req:Request,res:Response,next:NextFunction) =>{
+   
+   try{
+
+     const token  =  req.cookies?.refreshToken
+     if(!token){
+      throw new APIError(401,"Refresh Token missing")
+     }
+
+     jwt.verify(
+
+       token,
+       process.env.REFRESH_SECRET_TOKEN!,
+       async (error:Error | null,decoded:string | JwtPayload |undefined)=>{       
+         if(error){
+             if(error instanceof TokenExpiredError){
+                  throw new APIError(401,"Access Token Expired")
+            }else if (error instanceof JsonWebTokenError){
+                  throw new APIError(401,"Invalid Access Token")
+            }else{
+                  throw new APIError(401,"Error Verifying Access Token")
+            }
+         }
+        
+          if(!decoded || typeof decoded === "string"){
+               throw new APIError(401,"Error Access Token Payload Error")
+          }
+
+          const userID = decoded.userId as string;
+          const user =  await userModel.findById(userID)
+
+          if(!user){
+            throw new APIError(401,"user not found")
+          }
+
+          const newAcessToken = create_access_token(user._id.toString())
+          res.status(200).json({accessToken : newAcessToken,
+
+
+        user: {
+            id: user._id,
+            full_name: user.full_name,
+            email: user.email,
+            img: user.img,
+            createdAt: user.createAt,
+          },
+          })
+
+       }
+     )
+    
+   }catch(error){
+         console.log(error)
+          next(error)
+   }
+} 
+
+
+export const getAllUsers =  async(req:Request,res:Response,next:NextFunction) =>{
+
+    try{
+       const users = await userModel.find().select("-password")
+       res.status(201).json(users) 
+    }catch(error:any){
+       res.status(500).json({message:"internal server error"})
+    } 
+
+}
+
+
+export const logout = (req:Request,res:Response,next:NextFunction) =>{
+    
+   try{
+
+    const isProd = process.env.NODE_ENV === "production"
+
+    res.cookie("refreshToken","",{
+
+      httpOnly:true,
+      secure:isProd,
+      expires:new Date(0),
+      path:"/api/auth/refresh-token"
+    })
+    
+    res.status(200).json({message:"Logout successful"})
+
+   }catch(error:any){
+     next(error)
+   }
+
+}
--- chat-server/src/routes/user.routes.ts
+++ chat-server/src/routes/user.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
-import { signUp } from "../controller/auth.controller";
+import { getAllUsers, login, logout, refreshToken, signUp } from "../controller/auth.controller";
+import { authenticateToken } from "../middlewares/AuthenticateToken";
 
 const userRouter = Router()
 userRouter.post("/signup" , signUp)
+userRouter.post("/login",login)
+userRouter.get("/get",authenticateToken,getAllUsers)
+userRouter.post("/refresh-token",refreshToken)
+userRouter.post("/logout",logout)
 
 export default userRouter
